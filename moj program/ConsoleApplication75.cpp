e7cfd0516c8f410eba1069cdefada74483155a9e
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-04-02 08:26:19 +0200

    initial

---
+++ moj program/ConsoleApplication75.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication75.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello";
+	return 0;
+}
